31c6c4501be7954b6950e081ca9cfa5ca8bc4d16
karishma <karishmacarterjohnson@gmail.com>
2021-10-28 18:27:13 -0700

    update module exports to take file path, change function to return entire element instead of just link

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-# get each unit in unit.py
-# use unit title as folder
-
-# get each module 
-# use module name as subfolder
-# export files as pdf 
-
 from scrape.module import *
 from scrape.unit import *
 import os
 
-print(units_from_file('sample_files/test_home.html'))
-# loop through units
+hierarchy = units_from_file('sample_files/test_home.html')
+for unit, modules in hierarchy.items():
     # mkdir for the unit
-    # get links to modules 
-    # mkdir for each module
-    # for each page in module
-        # save file
+    for module_name, module_link in modules.items():
+        # mkdir for each module using module_name
+        # for each page in module
+        # define save path
+        module_pages = pages_from_file(module_link, path)
+        for page in module_pages:
+            # save file
+            print(page)
 
-#pdf_from_url('https://auth.galvanize.com/sign_in')    
-# pdf_from_file('sample_files/test_page.html')
-# module_files('https://learn-2.galvanize.com/cohorts/2836/blocks/1310/content_files/update-and-delete/update-and-delete.md')
+# pages_from_url('https://auth.galvanize.com/sign_in')    
--- scrape/module.py
+++ scrape/module.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import pdfkit
 
 def parse_html(html):
     '''
@@ -8,10 +9,9 @@ def parse_html(html):
     return: page's links
     '''
     soup = BeautifulSoup(html, "html.parser")
-    links = [result['href'] for result in soup.findAll('a', attrs = {'class': 'lesson'})]
-    return links
+    return soup.findAll('a', attrs = {'class': 'lesson'})
 
-def pdf_from_url(url):   
+def pages_from_url(url, path):   
     '''
     input: url from module
     return: page's links
@@ -19,11 +19,17 @@ def pdf_from_url(url):
     module_info = requests.get(url).content
     return parse_html(module_info)    
 
-def pdf_from_file(file):
+def pages_from_file(file, path):
     '''
     input: file handle from module
     return: page's links
     ''' 
     with open(file, 'r') as f:
         contents = f.read()
-    return parse_html(contents)
+   
+    for page in parse_html(contents):
+        # print(page['title'],page['href'])
+        '''
+        page_title = page['title']
+        pdfkit.from_url(page['href'], 'out.pdf')
+        '''
